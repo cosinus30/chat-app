6d999ecc502cefc4b53000b094508b604cde0690
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-06-18 15:34:31 +0300

    chore: add dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,62 @@
-import Classes from './App.module.css';
-import ChatHeader from './components/ChatHeader';
-import MessageContainer from './components/MessageContainer';
+import Classes from './App.module.css'
+import ChatHeader from './components/ChatHeader'
+import MessageContainer from './components/MessageContainer'
 
 function App() {
-  const messages = [{
-    messageText: "Message 1",
-    author: "Debra",
-    id: "1",
-    socketId: "user1",
-    time: "23.56"
-  },{
-    messageText: "Message 2",
-    author: "Dexter",
-    id: "2",
-    socketId: "user2",
-    time: "23.56"
-  },{
-    messageText: "Message 3",
-    author: "Debra",
-    id: "3",
-    socketId: "user1",
-    time: "23.56"
-  },{
-    messageText: "Message 4",
-    author: "Dexter",
-    id: "4",
-    socketId: "user2",
-    time: "23.56"
-  },{
-    messageText: "Message 5",
-    author: "Debra",
-    id: "5",
-    socketId: "user1",
-    time: "23.56"
-  }]
+  const messages = [
+    {
+      messageText: 'Message 1',
+      author: 'Debra',
+      id: '1',
+      socketId: 'user1',
+      time: '23.56',
+    },
+    {
+      messageText: 'Message 2',
+      author: 'Dexter',
+      id: '2',
+      socketId: 'user2',
+      time: '23.56',
+    },
+    {
+      messageText: 'Message 3',
+      author: 'Debra',
+      id: '3',
+      socketId: 'user1',
+      time: '23.56',
+    },
+    {
+      messageText: 'Message 4',
+      author: 'Dexter',
+      id: '4',
+      socketId: 'user2',
+      time: '23.56',
+    },
+    {
+      messageText: 'Message 5',
+      author: 'Debra',
+      id: '5',
+      socketId: 'user1',
+      time: '23.56',
+    },
+    {
+      messageText: 'Message 6',
+      author: 'Debra',
+      id: '6',
+      socketId: 'user1',
+      time: '23.56',
+    },
+  ]
 
   const isTyping = true
-  const usernameOfOpposite = "Dexter"
+  const usernameOfOpposite = 'Dexter'
 
   return (
     <div className={Classes.appContainer}>
       <ChatHeader isTyping={isTyping} username={usernameOfOpposite} />
-      <MessageContainer messages={messages}/>
+      <MessageContainer messages={messages} socketId={'user1'} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
