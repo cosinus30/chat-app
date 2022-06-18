60b655da86b9a4d1614c3bb89741bff082573153
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-06-18 15:34:19 +0300

    style: create the basis for message container

--- client/src/components/MessageContainer.js
+++ client/src/components/MessageContainer.js
@@ -1,10 +1,11 @@
 import Message from "./Message";
+import classes from './MessageContainer.module.css'
 
-function MessageContainer({messages = []}) {
+function MessageContainer({messages = [], socketId}) {
   return (
-    <div>
+    <div className={classes.messageContainer}>
       {messages.map(message => (
-        <Message key={message.id} message={message}/>
+        <Message key={message.id} message={message} isAuthorMe={socketId === message.socketId} />
       ))}
     </div>
   )
