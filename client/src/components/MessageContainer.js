f4661c821b58a64995743e7e83c680d9b4185bd0
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-06-18 17:20:21 +0300

    feat: enable scroll to bottom when new message arrives

--- client/src/components/MessageContainer.js
+++ client/src/components/MessageContainer.js
@@ -1,9 +1,16 @@
+import { useLayoutEffect, useRef } from "react";
 import Message from "./Message";
 import classes from './MessageContainer.module.css'
 
 function MessageContainer({messages = [], socketId}) {
+  const containerRef = useRef()
+
+  useLayoutEffect(() => {
+    containerRef.current.scrollTop = containerRef.current.scrollHeight
+  })
+
   return (
-    <div className={classes.messageContainer}>
+    <div ref={containerRef} className={classes.messageContainer}>
       {messages.map(message => (
         <Message key={message.id} message={message} isAuthorMe={socketId === message.socketId} />
       ))}
