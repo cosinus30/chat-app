1ede0e8003b043e344c503ae723d15fc3536892e
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-06-18 17:59:32 +0300

    feat: add nickname change functionality I moved the state to reducer function as I think it will be easier to add functionality along the way

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useReducer, useState} from 'react'
 import {io} from 'socket.io-client'
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid'
 
 import Classes from './App.module.css'
 import ChatHeader from './components/ChatHeader'
@@ -9,27 +9,69 @@ import TextInput from './components/TextInput'
 
 const SERVER = 'http://localhost:3001'
 
+function messagesReducer(state, action) {
+  switch (action.type) {
+    case 'NEW_MESSAGE': {
+      const {messageText, socketId, author} = action.message
+      const [verb, ...rest] = messageText.trim().split(' ')
+      if (verb === '/nick') {
+        const authorChanged = state.messages.map(message => {
+          return {...message, author: rest}
+        })
+        const newNickname = socketId === state.socket?.id ? rest : state.myNickname
+        const newOpponentNickname = socketId === state.socket?.id ? state.opponentNickname : rest
+        return {
+          ...state,
+          messages: authorChanged,
+          myNickname: newNickname,
+          opponentNickname: newOpponentNickname,
+        }
+      } else if (verb === '/think') {
+      } else if (verb === '/oops') {
+      } else if (verb === '/highlight') {
+      } else if (verb === '/fadelast') {
+      } else if (verb === '/countdown') {
+      } else {
+        return state
+      }
+    }
+    case 'INITIALIZE_SOCKET': {
+      return {
+        ...state,
+        socket: action.socket,
+      }
+      break
+    }
+    default: {
+      break
+    }
+  }
+}
+
 function App() {
-  const [messages, setMessages] = useState([])
-  const [socket, setSocket] = useState(null)
   const [messageText, setMessageText] = useState('')
-  const [nickname, setNickname] = useState('')
+  const [state, dispatch] = useReducer(messagesReducer, {
+    messages: [],
+    socket: null,
+    myNickname: '',
+    opponentNickname: '',
+  })
+  const {messages, socket, myNickname, opponentNickname} = state
 
   const isTyping = true
-  const usernameOfOpposite = 'Dexter'
 
   useEffect(() => {
     const newSocket = io(SERVER)
+    dispatch({type: 'INITIALIZE_SOCKET', socket: newSocket})
     newSocket.on('chat message', msg => {
-      setMessages(prevMessages => [...prevMessages, msg])
+      dispatch({type: 'NEW_MESSAGE', message: msg})
     })
-    setSocket(newSocket)
     return () => newSocket.close()
   }, [])
 
   const handleMessage = event => {
     if (event.key === 'Enter' && event.shiftKey === false) {
-      if(messageText.trim() === ''){
+      if (messageText.trim() === '') {
         event.preventDefault()
         return
       }
@@ -37,10 +79,10 @@ function App() {
         messageText: messageText,
         socketId: socket.id,
         sentTime: new Date(),
-        author: nickname,
-        id: uuidv4()
+        author: myNickname,
+        id: uuidv4(),
       }
-      setMessages(previousMessages => [...previousMessages, message])
+      dispatch({type: 'NEW_MESSAGE', message})
       setMessageText('')
       event.preventDefault()
       socket.emit('chat message', message)
@@ -49,9 +91,13 @@ function App() {
 
   return (
     <div className={Classes.appContainer}>
-      <ChatHeader isTyping={isTyping} username={usernameOfOpposite} />
+      <ChatHeader isTyping={isTyping} username={opponentNickname} />
       <MessageContainer messages={messages} socketId={socket?.id} />
-      <TextInput value={messageText} onChange={(event) => setMessageText(event.target.value)} onEnter={handleMessage} />
+      <TextInput
+        value={messageText}
+        onChange={event => setMessageText(event.target.value)}
+        onEnter={handleMessage}
+      />
     </div>
   )
 }
